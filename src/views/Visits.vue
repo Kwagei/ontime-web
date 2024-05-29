12412e02f386572ea5640f8997c3beef6d98cf2a
Dave Wonnah <wonnahdave@gmail.com>
2024-05-29 17:01:26 +0000

    Added few changes to visit list file on the table

--- src/views/Visits.vue
+++ src/views/Visits.vue
@@ -1,7 +1,8 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div class="">
-        <h1>This is the Visits page</h1>
-    </div>
+	<div class="">
+		<h1>This is the Visits page</h1>
+	</div>
 </template>
 
 <style></style>

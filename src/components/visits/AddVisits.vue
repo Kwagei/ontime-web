bed0934c2434c7ea54d264f5127c0395c8d1a96d
Dave Wonnah <wonnahdave@gmail.com>
2024-06-02 08:13:45 +0000

    working on functionality for creating visit

--- src/components/visits/AddVisits.vue
+++ src/components/visits/AddVisits.vue
@@ -1,17 +1,17 @@
 <template>
-  <div id="visit-view" class="d-flex flex-column container">
-    <div
-      class="d-flex justify-content-between align-items-center container p-0 mx-auto"
-      style="margin-top: 0.3rem"
-    >
-      <BreadCrumbs :breadCrumbs="activeBreadCrumbs" />
-    </div>
+    <div id="visitor-view" class="d-flex flex-column container">
+        <div
+            class="d-flex justify-content-between align-items-center container p-0 mx-auto"
+            style="margin-top: 0.3rem"
+        >
+            <BreadCrumbs :breadCrumbs="activeBreadCrumbs" />
+        </div>
 
-    <div
-      class="mt-4 form-control input"
-      style="margin: auto; padding: 3rem; width: 80%"
-    >
-      <!-- Button trigger modal -->
+        <div
+            class="mt-4 form-control input"
+            style="margin: auto; padding: 3rem"
+        >
+            <!-- Button trigger modal -->
             <button
                 type="button"
                 class="btn btn-primary"
@@ -62,195 +62,273 @@
                     </div>
                 </div>
             </div>
-            
-      <form class="row g-3 needs-validation" novalidate @submit.prevent="onSubmit">
-        <!-- Visitor Information -->
-        <div class="col-md-6">
-          <label for="visit" class="form-label is-required">Visitor</label>
-          <input
-            type="text"
-            class="form-control"
-            id="visit"
-            v-model="visit"
-            required
-          />
-        </div>
-        <div class="col-md-6">
-          <label for="arrival_time" class="form-label">Arrival Time</label>
-          <input
-            type="text"
-            class="form-control"
-            id="arrival_time"
-            placeholder="Readonly"
-            v-model="arrival_time"
-            disabled
-          />
-        </div>
 
-        <!-- Contact Information -->
-        <div class="col-md-6">
-          <label for="visit_contact" class="form-label is-required">Contact</label>
-          <input
-            type="text"
-            class="form-control"
-            id="visit_contact"
-            v-model="visit_contact"
-            required
-          />
-        </div>
-        <div class="col-md-6">
-          <label for="departural_time" class="form-label">Departure Time</label>
-          <input
-            type="text"
-            class="form-control"
-            id="departural_time"
-            placeholder="Readonly"
-            v-model="departural_time"
-            disabled
-          />
-        </div>
+            <form
+                class="row g-3 needs-validation"
+                novalidate
+                @submit.prevent="onSubmit"
+            >
+                <!-- NEW VISITOR -->
+                <div class="col-md-6">
+                    <label
+                        for="validationCustom01"
+                        class="form-label is-required"
+                        >Visitor<span class="visually-hidden">
+                            (required)</span
+                        ></label
+                    >
+                    <div class="input-group has-validation">
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="validationCustomNewVisitor"
+                            aria-describedby="inputGroupPrepend"
+                            v-model="new_visitor"
+                            required
+                        />
+                        <div class="invalid-feedback">
+                            Please provide a visitor name.
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- Arrival Time -->
+                <div class="col-md-6">
+                  <label for="validationCustomUsername" class="form-label"
+                  >Arrival Time</label
+                  >
+                  <input
+                  type="time"
+                  class="form-control"
+                  id="validationCustomVisitorArrivalTime"
+                  v-model="arrival_time"
+                  aria-describedby="inputGroupPrepend"
+                  disabled
+                  />
+                </div>
 
-        <!-- Host and Date Selection -->
-        <div class="col-md-6">
-          <label for="visit_host" class="form-label is-required">Host</label>
-          <button
-            type="button"
-            class="form-control btn btn-dropdown dropdown-toggle"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-            id="visit_host"
-          >
-            Select Host
-          </button>
-          <!-- Dropdown Menu for Host -->
-          <ul class="dropdown-menu dropdown-menu-end">
-            <li><button class="dropdown-item" type="button">Action</button></li>
-            <li><button class="dropdown-item" type="button">Another action</button></li>
-            <li><button class="dropdown-item" type="button">Something else here</button></li>
-          </ul>
-        </div>
-        <div class="col-md-6">
-          <label for="date" class="form-label">Date</label>
-          <input
-            type="date"
-            class="form-control"
-            id="date"
-            v-model="date"
-            disabled
-          />
-        </div>
+                <!-- PHONE NUMBER -->
+                <div class="col-md-6">
+                    <label
+                        for="validationCustom02"
+                        class="form-label is-required"
+                        >Phone number<span class="visually-hidden">
+                            (required)</span
+                        ></label
+                    >
+                    <div class="input-group has-validation">
+                        <input
+                            type="text"
+                            class="form-control"
+                            v-model="msisdn"
+                            id="validationCustomVisitorNumber"
+                            aria-describedby="inputGroupPrepend"
+                            required
+                        />
+                        <div class="invalid-feedback">
+                            Please provide a valid phone number.
+                        </div>
+                    </div>
+                </div>
+                <!-- Departural Time -->
+                <div class="col-md-6">
+                    <label for="validationCustom03" class="form-label"
+                        >Departural Time</label
+                    >
+                    <input
+                        type="time"
+                        class="form-control"
+                        id="validationCustomUsername"
+                        v-model="email"
+                        aria-describedby="inputGroupPrepend"
+                        disabled
+                    />
+                </div>
 
-        <!-- Room and Institution -->
-        <div class="col-md-6">
-          <label for="visit_room" class="form-label is-required">Room</label>
-          <button
-            type="button"
-            class="form-control btn btn-dropdown dropdown-toggle"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-            id="visit_room"
-          >
-            Select Room
-          </button>
-          <!-- Dropdown Menu for Room -->
-          <ul class="dropdown-menu dropdown-menu-end">
-            <li><button class="dropdown-item" type="button">Action</button></li>
-            <li><button class="dropdown-item" type="button">Another action</button></li>
-            <li><button class="dropdown-item" type="button">Something else here</button></li>
-          </ul>
-        </div>
-        <div class="col-md-6">
-          <label for="institution" class="form-label">Institution</label>
-          <input
-            type="text"
-            class="form-control"
-            id="visit_institution"
-            v-model="institution"
-          />
-        </div>
+                <!-- Host -->
+                <div class="col-md-6">
+                    <label
+                        for="validationCustom02"
+                        class="form-label is-required"
+                        >Host<span class="visually-hidden">
+                            (required)</span
+                        ></label
+                    >
+                    <div class="input-group has-validation">
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="validationCustom02"
+                            v-model="host_name"
+                            required
+                        />
+                        <div class="invalid-feedback">
+                            Please provide a host name.
+                        </div>
+                    </div>
+                </div>
 
-        <!-- Belongings and Purpose -->
-        <div class="col-md-6">
-          <label for="belonging" class="form-label is-required">Belonging</label>
-          <input
-            type="text"
-            class="form-control"
-            id="visit_belonging"
-            v-model="belonging"
-            required
-          />
-        </div>
-        <div class="col-md-12">
-          <label for="purpose" class="form-label is-required">Purpose</label>
-          <textarea
-            class="form-control"
-            id="purpose"
-            v-model="purpose"
-            required
-          ></textarea>
-        </div>
+                 <!-- Current Date -->
+                <div class="col-md-6 has-validation">
+                  <label for="validationCustomCurrentDate" class="form-label"
+                  >Date</label
+                  >
+                  <input
+                  type="Date"
+                  class="form-control"
+                  id="validationCustomVisitorArrivalTime"
+                  v-model="date"
+                  aria-describedby="inputGroupPrepend"
+                  required
+                  />
+                  <div class="invalid-feedback">
+                      Please provide the current date.
+                  </div>
+                </div>
 
-        <!-- Submit Button -->
-        <div class="col-12">
-          <button class="btn btn-primary mt-2" type="submit">
-            Save
-          </button>
+                <!-- Purpose -->
+                <div class="col-12">
+                    <label
+                        for="validationCustom02"
+                        class="form-label is-required"
+                        >Purpose<span class="visually-hidden">
+                            (required)</span
+                        ></label
+                    >
+                    <div class="input-group has-validation">
+                        <textarea
+                            class="form-control"
+                            id="validationCustom02"
+                            required
+                            v-model="purpose"
+                        ></textarea>
+                        <div class="invalid-feedback">
+                            Please provide an purpose.
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <button class="btn btn-primary mt-2" type="submit">
+                        Save
+                    </button>
+                </div>
+            </form>
         </div>
-      </form>
     </div>
-  </div>
 </template>
-
-<script setup>
-import { ref, defineEmits } from 'vue';
-import BreadCrumbs from '../BreadCrumbs.vue';
-
-const visit = ref('');
-const visit_contact = ref('');
-const arrival_time = ref('');
-const departural_time = ref('');
-const host = ref('');
-const date = ref('');
-const room = ref('');
-const institution = ref('');
-const belonging = ref('');
-const purpose = ref('');
-
-const emit = defineEmits(['visitFormSubmitted']);
-
-const onSubmit = () => {
-  const visits = {
-    visit: visit.value,
-    visit_contact: visit_contact.value,
-    arrival_time: arrival_time.value,
-    departural_time: departural_time.value,
-    host: host.value,
-    date: date.value,
-    room: room.value,
-    institution: institution.value,
-    belonging: belonging.value,
-    purpose: purpose.value,
-  };
-
-  emit('visitFormSubmitted', visits);
-  console.log(visits);
-};
-</script>
 
 <style scoped>
 .input {
-  border: 0.0125rem solid #ccc;
-  border-radius: 0.25rem !important;
+    border: 0.0125rem solid #ccc;
+    border-radius: 0.25rem !important;
+}
+</style>
+
+<script setup>
+import BreadCrumbs from "../BreadCrumbs.vue";
+import ModalTemplate from "../ModalTemplate.vue";
+import { registerVisitor } from "@/assets/js/index.js";
+
+import { ref, defineProps, onMounted } from "vue";
+
+const new_visitor = ref("");
+const arrival_time = ref("");
+const host_name = ref("");
+const msisdn = ref("");
+const email = ref("");
+const purpose = ref("");
+
+const emit = defineEmits(["visitorFormSubmitted"]);
+
+const onSubmit = async () => {
+    if (
+        !new_visitor.value ||
+        !host_name.value ||
+        !msisdn.value ||
+        !purpose.value
+    ) {
+        return;
+    }
+
+    const visitor = {
+        new_visitor: new_visitor.value,
+        arrival_time: arrival_time.value,
+        host_name: host_name.value,
+        msisdn: msisdn.value,
+        email: email.value,
+        purpose: purpose.value,
+    };
+
+    const response = await registerVisitor(visitor);
+
+    // if (!response.ok) {
+    //     console.log("");
+    //     onMounted(() => {
+    //         document.querySelector("#exampleModal").classList.toggle("show");
+    //     });
+    // }
+    console.log({ response });
+
+    emit("visitorFormSubmitted", visitor);
+};
+
+const activeBreadCrumbs = ref([]);
+
+const props = defineProps({
+    breadCrumbs: {
+        type: Array,
+        required: true,
+    },
+});
+
+activeBreadCrumbs.value = [...props.breadCrumbs, "new-visit"];
+
+const formValidation = onMounted(() => {
+    (() => {
+        "use strict";
+
+        const form = document.querySelector(".needs-validation");
+
+        form.addEventListener(
+            "submit",
+            (event) => {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                form.classList.add("was-validated");
+            },
+            false
+        );
+    })();
+});
+</script>
+
+<style scoped>
+#list-options {
+    padding: 0.6rem 0.5rem;
+    font-weight: 400;
+    border: 0.0125rem solid #ccc;
+    border-radius: 0.25rem !important;
+}
+svg {
+    height: 20px !important;
+    margin: 0 !important;
 }
 
-#visit-view {
-  padding-top: 2rem;
-  gap: 1.5rem;
+#new-visitor:hover {
+    color: white !important;
+}
+
+#visitor-view {
+    padding-top: 2rem;
+    gap: 1.5rem;
 }
 
 @media (min-width: 768px) and (max-width: 1440px) {
-  #visit-view {
-    padding: 1rem 3rem 0 3rem;
-  }
+    #visitor-view {
+        padding: 1rem 3rem 0 3rem;
+    }
 }
 </style>

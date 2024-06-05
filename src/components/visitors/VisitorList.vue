25a41936f0aa3bd3c0ea53a984368db4bd542729
peacebrowne <emmanuelbrowne751999@gmail.com>
2024-06-05 21:00:19 +0000

    Completed sorting for visitors

--- src/components/visitors/VisitorList.vue
+++ src/components/visitors/VisitorList.vue
@@ -106,6 +106,13 @@ sortTerm.value = "created_at";
 const directionTerm = defineModel("direction");
 directionTerm.value = "desc";
 
+// watch(
+//     () => directionTerms.value,
+//     (n, o) => {
+//         console.log;
+//     }
+// );
+
 watch(
     () => [searchTerms.value, sortTerm.value, directionTerm.value, start.value],
     async ([searchValue, sortValue, directionValue, startValue]) => {

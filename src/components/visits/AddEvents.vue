0f337557dcfb832f51fa327ae8c7e1cd0145e7e1
Dave Wonnah <wonnahdave@gmail.com>
2024-06-17 12:13:20 +0000

    can register a visit successfully

--- src/components/visits/AddEvents.vue
+++ src/components/visits/AddEvents.vue
@@ -1,13 +1,11 @@
 <template>
     <Modal :data="{ title, message, status }" />
     <div id="visit-view" class="d-flex flex-column container">
-        <div
-            class="d-flex justify-content-between align-items-center container p-0 mx-auto"
-            style="margin-top: 0.3rem"
-        >
+        <div class="d-flex justify-content-between align-items-center container p-0 mx-auto" style="margin-top: 0.3rem">
             <BreadCrumbs :breadCrumbs="activeBreadCrumbs" />
         </div>
 
+<<<<<<< HEAD
         <div
             class="mt-4 form-control input"
             style="margin: auto; padding: 3rem"
@@ -37,11 +35,26 @@
                     />
                     <div class="invalid-feedback">
                         Please provide a valid phone number.
+=======
+        <div class="mt-4 form-control input" style="margin: auto; padding: 3rem">
+            <form class="row g-3 needs-validation" novalidate @submit.prevent="onSubmit">
+                <!-- PHONE NUMBER -->
+                <div class="col-md-6">
+                    <label for="validationCustomVisitorNumber" class="form-label is-required">
+                        Phone number<span class="visually-hidden">(required)</span>
+                    </label>
+                    <div class="input-group has-validation">
+                        <input @blur="getVisitor" type="text" class="form-control" v-model="msisdn" id="validationCustomVisitorNumber" aria-describedby="inputGroupPrepend" required />
+                        <div class="invalid-feedback">
+                            Please provide a valid phone number.
+                        </div>
+>>>>>>> d239594 (can register a visit successfully)
                     </div>
                 </div>
             </div>
                 <!-- NEW VISITOR -->
                 <div class="col-md-6">
+<<<<<<< HEAD
                     <label
                         for="validationCustom01"
                         class="form-label is-required"
@@ -58,6 +71,13 @@
                             v-model="new_visitor"
                             required
                         />
+=======
+                    <label for="validationCustomNewVisitor" class="form-label is-required">
+                        Visitor<span class="visually-hidden">(required)</span>
+                    </label>
+                    <div class="input-group has-validation">
+                        <input type="text" class="form-control" id="validationCustomNewVisitor" aria-describedby="inputGroupPrepend" v-model="visitor" required />
+>>>>>>> d239594 (can register a visit successfully)
                         <div class="invalid-feedback">
                             Please provide a visitor name.
                         </div>
@@ -66,6 +86,7 @@
 
                 <!-- Host -->
                 <div class="col-md-6">
+<<<<<<< HEAD
                     <label
                         for="validationCustom02"
                         class="form-label is-required"
@@ -81,12 +102,25 @@
                             v-model="host_name"
                             required
                         />
+=======
+                    <label for="validationCustomHost" class="form-label is-required">
+                        Host<span class="visually-hidden">(required)</span>
+                    </label>
+                    <div class="input-group has-validation">
+                        <select v-model="host_name" class="form-select" id="validationCustomHost" required>
+                            <option value="" disabled>Select a host</option>
+                            <option v-for="option in options" :key="option.value" :value="option.value">
+                                {{ option.text }}
+                            </option>
+                        </select>
+>>>>>>> d239594 (can register a visit successfully)
                         <div class="invalid-feedback">
                             Please provide a host name.
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6">
+<<<<<<< HEAD
                     <label
                         for="validationCustom02"
                         class="form-label is-required"
@@ -102,6 +136,13 @@
                             v-model="belonging"
                             required
                         />
+=======
+                    <label for="validationCustomBelonging" class="form-label is-required">
+                        Belonging<span class="visually-hidden">(required)</span>
+                    </label>
+                    <div class="input-group has-validation">
+                        <input type="text" class="form-control" id="validationCustomBelonging" v-model="belonging" required />
+>>>>>>> d239594 (can register a visit successfully)
                         <div class="invalid-feedback">
                             Please provide a belonging name.
                         </div>
@@ -109,6 +150,7 @@
                 </div>
                 <!-- Institution -->
                 <div class="col-md-6">
+<<<<<<< HEAD
                     <label
                         for="validationCustom02"
                         class="form-label"
@@ -123,10 +165,18 @@
                             id="validationCustom02"
                             v-model="institution"
                         />
+=======
+                    <label for="validationCustomInstitution" class="form-label">
+                        Institution<span class="visually-hidden">(required)</span>
+                    </label>
+                    <div class="input-group has-validation">
+                        <input type="text" class="form-control" id="validationCustomInstitution" v-model="institution" />
+>>>>>>> d239594 (can register a visit successfully)
                     </div>
                 </div>
                 <!-- Room -->
                 <div class="col-md-6">
+<<<<<<< HEAD
                     <label
                         for="validationCustom02"
                         class="form-label is-required"
@@ -142,6 +192,13 @@
                             v-model="room"
                             required
                         />
+=======
+                    <label for="validationCustomRoom" class="form-label is-required">
+                        Room<span class="visually-hidden">(required)</span>
+                    </label>
+                    <div class="input-group has-validation">
+                        <input type="text" class="form-control" id="validationCustomRoom" v-model="room" required />
+>>>>>>> d239594 (can register a visit successfully)
                         <div class="invalid-feedback">
                             Please provide a room name.
                         </div>
@@ -149,6 +206,7 @@
                 </div>
                 <!-- Address -->
                 <div class="col-md-6">
+<<<<<<< HEAD
                     <label
                         for="validationCustom02"
                         class="form-label is-required"
@@ -164,6 +222,13 @@
                             v-model="visit_address"
                             required
                         />
+=======
+                    <label for="validationCustomAddress" class="form-label is-required">
+                        Address<span class="visually-hidden">(required)</span>
+                    </label>
+                    <div class="input-group has-validation">
+                        <input type="text" class="form-control" id="validationCustomAddress" v-model="visit_address" required />
+>>>>>>> d239594 (can register a visit successfully)
                         <div class="invalid-feedback">
                             Please provide a address name.
                         </div>
@@ -171,6 +236,7 @@
                 </div>
                 <!-- Purpose -->
                 <div class="col-12">
+<<<<<<< HEAD
                     <label
                         for="validationCustom02"
                         class="form-label is-required"
@@ -185,6 +251,13 @@
                             required
                             v-model="purpose"
                         ></textarea>
+=======
+                    <label for="validationCustomPurpose" class="form-label is-required">
+                        Purpose<span class="visually-hidden">(required)</span>
+                    </label>
+                    <div class="input-group has-validation">
+                        <textarea class="form-control" id="validationCustomPurpose" required v-model="purpose"></textarea>
+>>>>>>> d239594 (can register a visit successfully)
                         <div class="invalid-feedback">
                             Please provide an purpose.
                         </div>
@@ -208,6 +281,7 @@
 </style>
 
 <script setup>
+<<<<<<< HEAD
 import BreadCrumbs from "../BreadCrumbs.vue";
 import Modal from "../Modal.vue";
 import { registerVisit } from "@/assets/js/index.js";
@@ -216,8 +290,20 @@ import { ref, defineProps, onMounted } from "vue";
 
 const msisdn = ref("");
 const new_visitor = ref("");
+=======
+import { ref, defineProps, watch, onMounted } from "vue";
+import BreadCrumbs from "../BreadCrumbs.vue";
+import Modal from "../Modal.vue";
+import { registerVisit, getSingleVisitor, getUsers } from "@/assets/js/index.js";
+
+const msisdn = ref("");
+const visitor = ref("");
+const visitorId = ref("");
+>>>>>>> d239594 (can register a visit successfully)
 const host_name = ref("");
 const belonging = ref("");
+const options = ref([]);
+const host_id = ref("");
 const institution = ref("");
 const room = ref("");
 const visit_address = ref("");
@@ -226,6 +312,7 @@ const status = ref("");
 const message = ref("");
 const title = ref("");
 
+<<<<<<< HEAD
 const onSubmit = async () => {
     if (
         !msisdn.value ||
@@ -263,6 +350,8 @@ const onSubmit = async () => {
     }
 };
 
+=======
+>>>>>>> d239594 (can register a visit successfully)
 const activeBreadCrumbs = ref([]);
 
 const props = defineProps({
@@ -293,8 +382,100 @@ const formValidation = onMounted(() => {
             false
         );
     })();
+    getUserOptions();
 });
 
+<<<<<<< HEAD
+=======
+// function for inserting each username in the select element
+const getUserOptions = async () => {
+    try {
+        const users = await getUsers();
+        options.value = users.map(user => ({
+            value: user.id,
+            text: user.username
+        }));
+        console.log(options.value);
+    } catch (error) {
+        console.error("Error retrieving users:", error);
+    }
+};
+
+// function to get visitor bt MSISDN
+const getVisitor = async () => {
+    try {
+        const visitorData = await getSingleVisitor({ msisdn: msisdn.value });
+        if (!visitorData) {
+            visitor.value = "";
+            return;
+        }
+        visitor.value = `${visitorData.first_name} ${visitorData.last_name}`;
+        visitorId.value = visitorData.id;
+    } catch (error) {
+        console.error("Error retrieving visitor:", error);
+    }
+};
+
+// watching selected host name to update host ID
+watch(host_name, (newValue) => {
+    const selectedUser = options.value.find(option => option.value === newValue);
+    if (selectedUser) {
+        console.log(`Selected username: ${selectedUser.text}`);
+        host_id.value = selectedUser.value;
+        console.log(host_id.value);
+    }
+});
+
+const room_id = "01d95aba-31c4-48d3-8b03-2587d8bb5730";
+
+// function to validate form before it submit the form 
+const onSubmit = async () => {
+    if (!msisdn.value || !visitor.value || !host_name.value || !belonging.value || !room.value || !visit_address.value || !purpose.value) {
+        return;
+    }
+
+    // plitting text into array by using command as the deleminator 
+    const formatedItems = belonging.value.split(",").map(item => item.trim());
+
+    // require values for the submittion of the form
+    const visitData = {
+        visitor_id: visitorId.value,
+        institution: institution.value,
+        address: visit_address.value,
+        items: formatedItems,
+        room_id: room_id,
+        host_id: host_id.value,
+        purpose: purpose.value
+    };
+    console.log(visitData);
+    const response = await registerVisit(visitData);
+
+    console.log(response);
+    
+    const myModal = new boosted.Modal("#exampleModal", { backdrop: true });
+    if (!response.ok) {
+        myModal.show(document.querySelector("#toggleMyModal"));
+        status.value = "danger";
+        message.value = response.result.message;
+        title.value = "Error";
+    } else {
+        myModal.show(document.querySelector("#toggleMyModal"));
+        status.value = "success";
+        message.value = response.result.message;
+        title.value = "Success";
+    }
+    
+    visuallyHideModalBackdrop();
+};
+
+function visuallyHideModalBackdrop() {
+    const modalsBackdrops = document.querySelectorAll(".modal-backdrop");
+
+    if (modalsBackdrops.length) {
+        modalsBackdrops.forEach((modal) => modal.classList.add("visually-hidden"));
+    }
+}
+>>>>>>> d239594 (can register a visit successfully)
 </script>
 
 <style scoped>

0b4d430f65cb7c328280902e206fb9b0585550fe
Dave Wonnah <110246198+davewonnah@users.noreply.github.com>
2024-06-26 13:57:29 +0000

    Updated API_URL in index.js to use environment variable from vercel

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
 
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = process.env.VITE_API_URL;
 
 export const registerVisit = async (data) => {
     try {

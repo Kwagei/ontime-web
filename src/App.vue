4127e9cd23ddadb9244a291fa80330515038184e
galakpaigates <jeedoarkoi@gmail.com>
2024-07-03 13:33:10 +0000

    pulled datatable integration from development

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-	<div id="wrapper">
-		<div class="d-flex gap-4 px-0" style="height: 100%">
-			<aside class="flex-grow-0 bg-info">
-				<SideBar
-					v-model:activeSection="activeSection"
-					v-model:breadCrumbs="breadCrumbs"
-				/>
-			</aside>
-			<main class="flex-grow-1" style="background-color: #ececec">
-				<HeaderBar class="" :activeSection="activeSection" />
+    <div id="wrapper">
+        <div class="d-flex gap-4 px-0" style="height: 100%">
+            <aside class="flex-grow-0 bg-info">
+                <SideBar
+                    v-model:activeSection="activeSection"
+                    v-model:breadCrumbs="breadCrumbs"
+                />
+            </aside>
+            <main class="flex-grow-1" style="background-color: #ececec">
+                <HeaderBar class="" :activeSection="activeSection" />
 
-				<section class="container">
-					<RouterView :breadCrumbs="breadCrumbs" />
-				</section>
-			</main>
-		</div>
-	</div>
+                <section class="container">
+                    <RouterView :breadCrumbs="breadCrumbs" />
+                </section>
+            </main>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -31,32 +31,32 @@ const breadCrumbs = ref([]);
 
 <style>
 #wrapper {
-	height: 100vh;
-	width: 100vw;
+    height: 100vh;
+    width: 100vw;
 }
 
 /* Section Scrolling Media Query */
 @media (min-width: 1700px) and (min-height: 920px) {
-	#wrapper {
-		position: fixed;
-	}
+    #wrapper {
+        position: fixed;
+    }
 }
 
 aside {
-	width: 20rem;
-	height: 100vh;
-	position: fixed;
-	z-index: 1700;
+    width: 20rem;
+    height: 100vh;
+    position: fixed;
+    z-index: 1700;
 }
 
 main {
-	margin-left: 20rem;
-	overflow-y: scroll;
-	padding-bottom: 3rem;
+    margin-left: 20rem;
+    overflow-y: scroll;
+    padding-bottom: 3rem;
 }
 
 .modal-backdrop {
-	display: none !important;
+    display: none !important;
 }
 
 section {
@@ -64,20 +64,20 @@ section {
 }
 
 @media (max-width: 1440px) {
-	aside {
-		width: 15rem;
-	}
+    aside {
+        width: 15rem;
+    }
 
-	main {
-		margin-left: 15rem;
-	}
+    main {
+        margin-left: 15rem;
+    }
 
-	section {
-		padding: 0 2rem !important;
-	}
+    section {
+        padding: 0 2rem !important;
+    }
 
-	header {
-		padding: 0 1.275rem !important;
-	}
+    header {
+        padding: 0 1.275rem !important;
+    }
 }
 </style>
--- src/components/events/EventsTable.vue
+++ src/components/events/EventsTable.vue
@@ -1,18 +1,10 @@
 <template>
-	<div
-		class="table-responsive container p-0 d-flex flex-column"
-		style="gap: 0.7rem"
-	>
-		<div>
-			<DataTable
-				id="eventssTable"
-				class="display w-100 table"
-				:columns="columns"
-				:options="options"
-				ref="table"
-			/>
-		</div>
-	</div>
+    <div
+        class="table-responsive container p-0 d-flex flex-column"
+        style="gap: 0.9rem"
+    >
+        <table id="eventTable" class="table table-striped w-100"></table>
+    </div>
 </template>
 
 <script setup>
@@ -98,44 +90,113 @@ const options = {
 const router = useRouter();
 const MAX_DETAIL_LEN = 30;
 
-const table = ref();
-
-onMounted(() => {
-	handleEventDetail();
+const props = defineProps({
+    refresh: Boolean,
 });
 
-const handleEventDetail = () => {
-	const dt = table.value.dt;
-	dt.on("click", "tr", function () {
-		const { id } = dt.row(this).data();
-		displayEventPage(id);
-	});
+const emit = defineEmits(["refreshComplete"]);
+
+onMounted(async () => {});
+
+// Watch Refresh Prop to refresh events accordingly
+watch(
+    () => props.refresh,
+    async (n) => {
+        if (n) {
+            emit("refreshComplete");
+        }
+    }
+);
+
+const initializeDataTable = () => {
+    dataTable.value = new DataTables("#eventTable", {
+        serverSide: true,
+        ajax: {
+            url: `http://localhost:3000/api/events`,
+            type: "GET",
+            data: (query) => {
+                const order =
+                    query.columns[query.order[0].column].data === "date"
+                        ? "date_time"
+                        : query.columns[query.order[0].column].data;
+                return {
+                    start: query.start,
+                    limit: query.length,
+                    search: query.search.value,
+                    sort: order,
+                    direction: query.order[0].dir,
+                };
+            },
+            dataSrc: (json) => {
+                json.data.forEach((event) => {
+                    event.details = formatDetails(event.details);
+                    event.start_date = formatDate(event.start_date);
+                    event.end_date = formatDate(event.end_date);
+                });
+
+                events.value = json.data;
+                loader.value = false;
+                return events.value;
+            },
+            error: (xhr, error, thrown) => {
+                console.log("Error fetching data:", error);
+            },
+        },
+        columns: [
+            { data: "title", title: "Title" },
+            { data: "host", title: "Host" },
+            { data: "start_date", title: "Start Date" },
+            { data: "end_date", title: "End Date" },
+            { data: "facilitator", title: "Facilitator" },
+            { data: "event_type", title: "Type" },
+            { data: "details", title: "Details" },
+        ],
+        responsive: true,
+        lengthMenu: [10, 25, 50, 100],
+        language: {
+            searchPlaceholder: "Search ...",
+            search: "",
+        },
+    });
+
+    dataTable.value.on("click", "tr", function () {
+        const rowData = dataTable.value.row(this).data();
+        if (rowData) {
+            console.log(rowData.id);
+            displayEventPage(rowData.id);
+        }
+    });
 };
 
 const formatDate = (date) => {
-	const rawDate = new Date(date);
-	return rawDate.toString().split(" 0")[0];
+    const rawDate = new Date(date);
+
+    return rawDate.toString().split(" 0")[0];
 };
 
+onMounted(async () => {
+    initializeDataTable();
+});
+
 const formatDetails = (detail) => {
-	return detail.length > MAX_DETAIL_LEN
-		? `${detail.slice(0, MAX_DETAIL_LEN)}...`
-		: detail;
+    return detail.length > MAX_DETAIL_LEN
+        ? `${detail.slice(0, MAX_DETAIL_LEN)}...`
+        : detail;
 };
 
 function displayEventPage(eventId) {
-	router.push({ name: "specific-event", params: { id: eventId } });
+    router.push({ name: "specific-event", params: { id: eventId } });
 }
 </script>
 
 <style scoped>
 .cursorPointer {
-	cursor: pointer;
+    cursor: pointer;
 }
 
 th,
 td {
-	padding: 0.9rem;
-	font-size: 0.9rem;
+    padding: 0.9rem;
+    font-size: 0.9rem;
 }
 </style>

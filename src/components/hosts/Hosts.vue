7c504f52f038384c20d9f54d45ea4d440415bd24
peacebrowne <emmanuelbrowne751999@gmail.com>
2024-07-10 21:52:04 +0000

    Udated the visits table with the feedback from Nathan, which are to add status and action columns and join date and arrival_time as one

--- src/components/hosts/Hosts.vue
+++ src/components/hosts/Hosts.vue
@@ -1,124 +1,124 @@
 <template>
-    <Modal :data="{ title, message, status }" />
-    <div id="visitor-view" class="d-flex flex-column container">
-        <div
-            class="d-flex justify-content-between align-items-center container p-0 mx-auto"
-            style="margin-top: 0.3rem"
-        >
-            <BreadCrumbs :breadCrumbs="activeBreadCrumbs" />
-        </div>
+	<Modal :data="{ title, message, status }" />
+	<div id="visitor-view" class="d-flex flex-column container">
+		<div
+			class="d-flex justify-content-between align-items-center container p-0 mx-auto"
+			style="margin-top: 0.3rem"
+		>
+			<BreadCrumbs :breadCrumbs="activeBreadCrumbs" />
+		</div>
 
-        <div
-            class="mt-4 form-control input"
-            style="margin: auto; padding: 3rem"
-        >
-            <form
-                class="row g-3 needs-validation"
-                novalidate
-                @submit.prevent="onSubmit"
-            >
-                <!-- NAME -->
-                <div class="col-md-6">
-                    <label for="name" class="form-label is-required"
-                        >Name<span class="visually-hidden">
-                            (required)</span
-                        ></label
-                    >
-                    <div class="input-group has-validation">
-                        <input
-                            type="text"
-                            class="form-control"
-                            id="name"
-                            aria-describedby="inputGroupPrepend"
-                            v-model="name"
-                            required
-                        />
-                        <div class="invalid-feedback">
-                            Please provide a host name.
-                        </div>
-                    </div>
-                </div>
+		<div
+			class="mt-4 form-control input"
+			style="margin: auto; padding: 3rem"
+		>
+			<form
+				class="row g-3 needs-validation"
+				novalidate
+				@submit.prevent="onSubmit"
+			>
+				<!-- NAME -->
+				<div class="col-md-6">
+					<label for="name" class="form-label is-required"
+						>Name<span class="visually-hidden">
+							(required)</span
+						></label
+					>
+					<div class="input-group has-validation">
+						<input
+							type="text"
+							class="form-control"
+							id="name"
+							aria-describedby="inputGroupPrepend"
+							v-model="name"
+							required
+						/>
+						<div class="invalid-feedback">
+							Please provide a host name.
+						</div>
+					</div>
+				</div>
 
-                <!-- PHONE NUMBER -->
-                <div class="col-md-6">
-                    <label for="phone_number" class="form-label is-required"
-                        >Phone number<span class="visually-hidden">
-                            (required)</span
-                        ></label
-                    >
-                    <div class="input-group has-validation">
-                        <input
-                            type="tel"
-                            :class="[
-                                validMsisdn && 'validated',
-                                'form-control',
-                            ]"
-                            v-model="msisdn"
-                            id="phone_number"
-                            aria-describedby="inputGroupPrepend"
-                            required
-                            @blur="contactValidation"
-                        />
-                        <div
-                            :class="[
-                                'invalid-feedback',
-                                validMsisdn && 'show-feedback',
-                            ]"
-                        >
-                            {{ validMsisdnMessage }}
-                        </div>
-                    </div>
-                    <div id="emailHelp" class="form-text">
-                        For example: 0778456789
-                    </div>
-                </div>
+				<!-- PHONE NUMBER -->
+				<div class="col-md-6">
+					<label for="phone_number" class="form-label is-required"
+						>Phone number<span class="visually-hidden">
+							(required)</span
+						></label
+					>
+					<div class="input-group has-validation">
+						<input
+							type="tel"
+							:class="[
+								validMsisdn && 'validated',
+								'form-control',
+							]"
+							v-model="msisdn"
+							id="phone_number"
+							aria-describedby="inputGroupPrepend"
+							required
+							@blur="contactValidation"
+						/>
+						<div
+							:class="[
+								'invalid-feedback',
+								validMsisdn && 'show-feedback',
+							]"
+						>
+							{{ validMsisdnMessage }}
+						</div>
+					</div>
+					<div id="emailHelp" class="form-text">
+						For example: 0778456789
+					</div>
+				</div>
 
-                <!-- HOST TYPE -->
-                <div class="">
-                    <label for="host_type" class="form-label is-required"
-                        >Host Type<span class="visually-hidden">
-                            (required)</span
-                        ></label
-                    >
-                    <div class="form-check mb-0">
-                        <input class="form-check-input" type="checkbox" />
-                        <label for="">Individual</label>
-                    </div>
-                    <div class="form-check mb-0">
-                        <input class="form-check-input" type="checkbox" />
-                        <label for="">Company</label>
-                    </div>
-                </div>
+				<!-- HOST TYPE -->
+				<div class="">
+					<label for="host_type" class="form-label is-required"
+						>Host Type<span class="visually-hidden">
+							(required)</span
+						></label
+					>
+					<div class="form-check mb-0">
+						<input class="form-check-input" type="checkbox" />
+						<label for="">Individual</label>
+					</div>
+					<div class="form-check mb-0">
+						<input class="form-check-input" type="checkbox" />
+						<label for="">Company</label>
+					</div>
+				</div>
 
-                <!-- Details -->
-                <div class="col-md-6">
-                    <label for="detailsTextarea" class="form-label"
-                        >Details</label
-                    >
-                    <textarea
-                        placeholder="Enter details..."
-                        class="form-control"
-                        id="detailsTextarea"
-                        v-model="details"
-                        rows="4"
-                    ></textarea>
-                </div>
+				<!-- Details -->
+				<div class="col-md-6">
+					<label for="detailsTextarea" class="form-label"
+						>Details</label
+					>
+					<textarea
+						placeholder="Enter details..."
+						class="form-control"
+						id="detailsTextarea"
+						v-model="details"
+						rows="4"
+					></textarea>
+				</div>
 
-                <div class="col-md-12">
-                    <button
-                        type="submit"
-                        class="btn btn-primary"
-                        style="
-                            padding: 0.7rem 2rem !important;
-                            font-weight: 600;
-                        "
-                    >
-                        {{ buttonLabel }}
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
+				<div class="col-md-12">
+					<button
+						type="submit"
+						class="btn btn-primary"
+						style="
+							padding: 0.7rem 2rem !important;
+							font-weight: 600;
+						"
+					>
+						{{ buttonLabel }}
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </template>
 
 <script setup>
@@ -151,135 +151,135 @@ const formStatus = tem.pop();
 
 // Functions
 const onSubmit = async () => {
-    if (!name.value || !msisdn.value) {
-        return;
-    }
+	if (!name.value || !msisdn.value) {
+		return;
+	}
 
-    const host = {
-        name: name.value,
-        msisdn:
-            msisdn.value[0] == "0"
-                ? `231${msisdn.value.slice(1)}`
-                : msisdn.value,
-        details: details.value,
-    };
+	const host = {
+		name: name.value,
+		msisdn:
+			msisdn.value[0] == "0"
+				? `231${msisdn.value.slice(1)}`
+				: msisdn.value,
+		details: details.value,
+	};
 
-    const response = formStatus.startsWith("new")
-        ? await registerHost(host)
-        : await editHost(hostInfo.id, host);
+	const response = formStatus.startsWith("new")
+		? await registerHost(host)
+		: await editHost(hostInfo.id, host);
 
-    console.log(response);
+	console.log(response);
 
-    const myModal = new boosted.Modal("#exampleModal", { backdrop: true });
-    myModal.show(document.querySelector("#toggleMyModal"));
-    status.value = response.ok ? "success" : "danger";
-    message.value = response.result.message;
-    title.value = response.ok ? "Success" : "Error";
+	const myModal = new boosted.Modal("#exampleModal", { backdrop: true });
+	myModal.show(document.querySelector("#toggleMyModal"));
+	status.value = response.ok ? "success" : "danger";
+	message.value = response.result.message;
+	title.value = response.ok ? "Success" : "Error";
 
-    visuallyHideModalBackdrop();
+	visuallyHideModalBackdrop();
 
-    // Reset form if the response is successful
-    if (response.ok) {
-        resetForm();
-    }
+	// Reset form if the response is successful
+	if (response.ok) {
+		resetForm();
+	}
 };
 
 const fetchHost = async () => {
-    if (formStatus.startsWith("edit")) {
-        buttonLabel.value = "Update";
-        const id = breadCrumbs.value[1];
-        hostInfo = await getHosts({ id });
+	if (formStatus.startsWith("edit")) {
+		buttonLabel.value = "Update";
+		const id = breadCrumbs.value[1];
+		hostInfo = await getHosts({ id });
 
-        name.value = hostInfo.name;
-        msisdn.value = hostInfo.msisdn;
-        details.value = hostInfo.details;
-    }
+		name.value = hostInfo.name;
+		msisdn.value = hostInfo.msisdn;
+		details.value = hostInfo.details;
+	}
 };
 
 const visuallyHideModalBackdrop = () => {
-    document
-        .querySelectorAll(".modal-backdrop")
-        .forEach((modal) => modal.classList.add("visually-hidden"));
+	document
+		.querySelectorAll(".modal-backdrop")
+		.forEach((modal) => modal.classList.add("visually-hidden"));
 };
 
 const validMsisdn = ref(false);
 const validMsisdnMessage = ref("Please provide a phone number");
 
 const contactValidation = () => {
-    if (!msisdn.value) {
-        validMsisdn.value = false;
-        validMsisdnMessage.value = "Please provide a phone number";
+	if (!msisdn.value) {
+		validMsisdn.value = false;
+		validMsisdnMessage.value = "Please provide a phone number";
 
-        return;
-    }
-    const isvalid = msisdnValidation([msisdn.value]);
+		return;
+	}
+	const isvalid = msisdnValidation([msisdn.value]);
 
-    if (!isvalid.valid) {
-        validMsisdn.value = true;
-        validMsisdnMessage.value = isvalid.message;
-    } else {
-        validMsisdn.value = false;
-    }
+	if (!isvalid.valid) {
+		validMsisdn.value = true;
+		validMsisdnMessage.value = isvalid.message;
+	} else {
+		validMsisdn.value = false;
+	}
 };
 
 const resetForm = () => {
-    name.value = "";
-    msisdn.value = "";
-    details.value = "";
-    buttonLabel.value = "Save";
+	name.value = "";
+	msisdn.value = "";
+	details.value = "";
+	buttonLabel.value = "Save";
 
-    // Remove validation classes
-    const form = document.querySelector(".needs-validation");
-    form.classList.remove("was-validated");
+	// Remove validation classes
+	const form = document.querySelector(".needs-validation");
+	form.classList.remove("was-validated");
 };
 
 // Lifecycle Hooks
 onMounted(() => {
-    fetchHost();
+	fetchHost();
 
-    const form = document.querySelector(".needs-validation");
-    form.addEventListener(
-        "submit",
-        (event) => {
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            form.classList.add("was-validated");
-        },
-        false
-    );
+	const form = document.querySelector(".needs-validation");
+	form.addEventListener(
+		"submit",
+		(event) => {
+			if (!form.checkValidity()) {
+				event.preventDefault();
+				event.stopPropagation();
+			}
+			form.classList.add("was-validated");
+		},
+		false
+	);
 });
 </script>
 
 <style scoped>
 .show-feedback {
-    display: flex;
+	display: flex;
 }
 
 .validated {
-    border-color: var(--bs-form-invalid-border-color);
+	border-color: var(--bs-form-invalid-border-color);
 }
 
 #list-options {
-    padding: 0.6rem 0.5rem;
-    font-weight: 400;
-    border: 0.0125rem solid #ccc;
-    border-radius: 0.25rem !important;
+	padding: 0.6rem 0.5rem;
+	font-weight: 400;
+	border: 0.0125rem solid #ccc;
+	border-radius: 0.25rem !important;
 }
 svg {
-    height: 20px !important;
-    margin: 0 !important;
+	height: 20px !important;
+	margin: 0 !important;
 }
 
 #visitor-view {
-    padding-top: 2rem;
-    gap: 1.5rem;
+	padding-top: 2rem;
+	gap: 1.5rem;
 }
 
 @media (min-width: 768px) and (max-width: 1440px) {
-    #visitor-view {
-        padding: 1rem 3rem 0 3rem;
-    }
+	#visitor-view {
+		padding: 1rem 3rem 0 3rem;
+	}
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -20,135 +20,134 @@ import Users from "../views/Users.vue";
 
 // Dashboard Components
 import Dashboard from "../views/Dashboard.vue";
-import Host from "@/components/Host.vue";
-import Room from "@/components/Room.vue";
-
 // Dependecies Imports
 import { createRouter, createWebHistory } from "vue-router";
 import AddParticipant from "@/components/events/AddParticipant.vue";
+import Hosts from "@/components/hosts/Hosts.vue";
+import Room from "@/components/Room.vue";
 
 const routes = [
 	{
 		path: "",
-		redirect: "/visits"
+		redirect: "/visits",
 	},
-    {
-        path: "/dashboard",
-        component: Dashboard,
-        name: "dashboard",
-    },
-    {
-        path: "/visitors",
-        children: [
-            {
-                path: "",
-                component: Visitors,
-                name: "visitors",
-            },
-            {
-                path: "new-visitor",
-                component: AddVisitors,
-                name: "add-visitor",
-                prop: true,
-            },
-            {
-                path: ":id",
-                children: [
-                    {
-                        path: "",
-                        component: VisitorDetail,
-                        name: "visitorDetail",
-                    },
-                    {
-                        path: "edit-visitor",
-                        component: AddVisitors,
-                        name: "edit-visitor",
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        path: "/visits",
-        children: [
-            {
-                path: "",
-                component: Visits,
-                name: "visits",
-            },
-            {
-                path: "new-visit",
-                component: AddVisits,
-                name: "add-visit",
-            },
-            {
-                path: "purpose-meeting",
-                component: AddMeeting,
-                name: "add-meeting",
-            },
-            {
-                path: "purpose-event",
-                component: AddEvents,
-                name: "visits-event",
-            },
-            {
-                path: "purpose-workspace",
-                component: AddWorkspace,
-                name: "add-workspace",
-            },
-        ],
-    },
-    {
-        path: "/users",
-        component: Users,
-        name: "users",
-    },
-    {
-        path: "/events",
-        children: [
-            { path: "", component: Events, name: "events" },
-            {
-                path: "/events/add-event",
-                component: AddEvent,
-                name: "add-event",
-            },
-            {
-                path: "/events/:id",
-                children: [
-                    {
-                        path: "",
-                        component: Event,
-                        name: "specific-event",
-                    },
-                    {
-                        path: "edit-event",
-                        component: AddEvent,
-                        name: "specific-event-participants",
-                    },
-                    {
-                        path: "add-participant",
-                        component: AddParticipant,
-                        name: "add-event-participant",
-                    },
-                ],
-            },
-            {
-                path: "new-host",
-                component: Host,
-                name: "new-host",
-            },
-            {
-                path: "new-room",
-                component: Room,
-                name: "new-room",
-            },
-        ],
-    },
+	{
+		path: "/dashboard",
+		component: Dashboard,
+		name: "dashboard",
+	},
+	{
+		path: "/visitors",
+		children: [
+			{
+				path: "",
+				component: Visitors,
+				name: "visitors",
+			},
+			{
+				path: "new-visitor",
+				component: AddVisitors,
+				name: "add-visitor",
+				prop: true,
+			},
+			{
+				path: ":id",
+				children: [
+					{
+						path: "",
+						component: VisitorDetail,
+						name: "visitorDetail",
+					},
+					{
+						path: "edit-visitor",
+						component: AddVisitors,
+						name: "edit-visitor",
+					},
+				],
+			},
+		],
+	},
+	{
+		path: "/visits",
+		children: [
+			{
+				path: "",
+				component: Visits,
+				name: "visits",
+			},
+			{
+				path: "new-visit",
+				component: AddVisits,
+				name: "add-visit",
+			},
+			{
+				path: "purpose-meeting",
+				component: AddMeeting,
+				name: "add-meeting",
+			},
+			{
+				path: "purpose-event",
+				component: AddEvents,
+				name: "visits-event",
+			},
+			{
+				path: "purpose-workspace",
+				component: AddWorkspace,
+				name: "add-workspace",
+			},
+		],
+	},
+	{
+		path: "/users",
+		component: Users,
+		name: "users",
+	},
+	{
+		path: "/events",
+		children: [
+			{ path: "", component: Events, name: "events" },
+			{
+				path: "/events/add-event",
+				component: AddEvent,
+				name: "add-event",
+			},
+			{
+				path: "/events/:id",
+				children: [
+					{
+						path: "",
+						component: Event,
+						name: "specific-event",
+					},
+					{
+						path: "edit-event",
+						component: AddEvent,
+						name: "specific-event-participants",
+					},
+					{
+						path: "add-participant",
+						component: AddParticipant,
+						name: "add-event-participant",
+					},
+				],
+			},
+			{
+				path: "new-host",
+				component: Hosts,
+				name: "new-host",
+			},
+			{
+				path: "new-room",
+				component: Room,
+				name: "new-room",
+			},
+		],
+	},
 ];
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes,
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes,
 });
 
 export default router;

8f7b316f0797b8723456573cedd4af1ec56383bd
galakpaigates <jeedoarkoi@gmail.com>
2024-10-14 20:11:35 +0000

    completed info updates on Ongoing Events Grid when checking in for an event | other minor updates

--- src/components/checkIn/AddEvents.vue
+++ src/components/checkIn/AddEvents.vue
@@ -31,7 +31,7 @@
                 </button>
 
                 <!-- Loader -->
-                <div v-if="loading" class="w-25 text-center ms-2 pt-5">
+                <div v-if="loading" class="w-100 text-center ms-2 pt-5">
                     <div class="spinner-border" role="status">
                         <span class="visually-hidden">Loading...</span>
                     </div>
@@ -68,12 +68,52 @@
                             @click="updateEventTerm(event, idx)"
                         >
                             <Icons
-                                :height="'68%'"
-                                :width="'70%'"
+                                :height="'58%'"
+                                :width="'60%'"
                                 class="optionIcon"
                                 icon="calendar-event-agenda"
                             />
                             <h3>{{ event.title }}</h3>
+                            <div
+                                class="d-flex flex-column gap-2 w-100 text-start text text-secondary"
+                            >
+                                <!-- Start & End Date -->
+                                <div class="d-flex flex-column gap-1">
+                                    <span>
+                                        <Icons icon="clock" /> Timespan
+                                    </span>
+                                    <span class="fw-bold">
+                                        {{
+                                            formatDateTime(event.startDate, {
+                                                date: true,
+                                            })
+                                        }}
+                                        to<br />
+                                        {{
+                                            formatDateTime(event.endDate, {
+                                                date: true,
+                                            })
+                                        }}
+                                    </span>
+                                </div>
+                                <!-- Facilitator -->
+                                <div class="d-flex flex-column gap-1">
+                                    <span>
+                                        <Icons icon="administrator" />
+                                        Facilitator
+                                    </span>
+                                    <span class="fw-bold">
+                                        {{ event.facilitator }}
+                                    </span>
+                                </div>
+                                <!-- Type -->
+                                <div class="d-flex flex-column gap-1">
+                                    <span> <Icons icon="apps" /> Type </span>
+                                    <span class="fw-bold">{{
+                                        event.type
+                                    }}</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <router-link to="/events/add-event">
@@ -125,6 +165,7 @@ import {
     removeClass,
     showModal,
     formatMsisdn,
+    formatDateTime,
 } from "@/util/util";
 import DataTable from "datatables.net-vue3";
 import DataTablesCore from "datatables.net";
@@ -237,13 +278,13 @@ const dataTableOptions = ref({
         emptyTable: `
 			<div class="d-flex flex-column justify-content-center align-items-center gap-3 p-4" >
 				<svg style="width: 5rem; height: 5rem;" width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" fill-rule="evenodd" d="M33.8 87.5V69.684A10.02 10.02 0 0126.3 60V48.75a18.7 18.7 0 011.763-7.943 16.64 16.64 0 01-10.72-4.533A15.29 15.29 0 008.8 50v11.912C8.8 65.81 12.001 69 15.9 69v18.5a4.965 4.965 0 004.959 5h14.857a7.47 7.47 0 01-1.916-5m48.958-51.226a16.64 16.64 0 01-10.72 4.533A18.7 18.7 0 0173.8 48.75V60a10.02 10.02 0 01-7.5 9.684V87.5a7.47 7.47 0 01-1.916 5h14.857a4.965 4.965 0 004.959-5V69c3.899 0 7.1-3.19 7.1-7.088V50a15.29 15.29 0 00-8.542-13.726M71.3 12.5a12.44 12.44 0 00-6.538 1.845q.135.286.26.579a16.24 16.24 0 01-2.416 16.644l.723.356a18.8 18.8 0 016.668 5.509A12.5 12.5 0 1071.3 12.5m-7.5 8.75A13.75 13.75 0 1150.05 7.5 13.75 13.75 0 0163.8 21.25m-1.576 12.916a17.747 17.747 0 01-24.348 0A16.25 16.25 0 0028.8 48.75V60a7.5 7.5 0 007.5 7.5v20a5 5 0 005 5h17.5a5 5 0 005-5v-20a7.5 7.5 0 007.5-7.5V48.75a16.25 16.25 0 00-9.076-14.584m-32.12 3.266a18.8 18.8 0 016.667-5.508l.723-.357a16.27 16.27 0 01-2.416-16.643q.125-.292.26-.58A12.5 12.5 0 1028.8 37.5a13 13 0 001.304-.067Z"/></svg>
-				No match!
+				No Event Participant
 			</div>
 		`,
         zeroRecords: `
 			<div class="d-flex flex-column justify-content-center align-items-center gap-3 p-4" >
 				<svg style="width: 5rem; height: 5rem;" width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><path fill="#000000" fill-rule="evenodd" d="M33.8 87.5V69.684A10.02 10.02 0 0126.3 60V48.75a18.7 18.7 0 011.763-7.943 16.64 16.64 0 01-10.72-4.533A15.29 15.29 0 008.8 50v11.912C8.8 65.81 12.001 69 15.9 69v18.5a4.965 4.965 0 004.959 5h14.857a7.47 7.47 0 01-1.916-5m48.958-51.226a16.64 16.64 0 01-10.72 4.533A18.7 18.7 0 0173.8 48.75V60a10.02 10.02 0 01-7.5 9.684V87.5a7.47 7.47 0 01-1.916 5h14.857a4.965 4.965 0 004.959-5V69c3.899 0 7.1-3.19 7.1-7.088V50a15.29 15.29 0 00-8.542-13.726M71.3 12.5a12.44 12.44 0 00-6.538 1.845q.135.286.26.579a16.24 16.24 0 01-2.416 16.644l.723.356a18.8 18.8 0 016.668 5.509A12.5 12.5 0 1071.3 12.5m-7.5 8.75A13.75 13.75 0 1150.05 7.5 13.75 13.75 0 0163.8 21.25m-1.576 12.916a17.747 17.747 0 01-24.348 0A16.25 16.25 0 0028.8 48.75V60a7.5 7.5 0 007.5 7.5v20a5 5 0 005 5h17.5a5 5 0 005-5v-20a7.5 7.5 0 007.5-7.5V48.75a16.25 16.25 0 00-9.076-14.584m-32.12 3.266a18.8 18.8 0 016.667-5.508l.723-.357a16.27 16.27 0 01-2.416-16.643q.125-.292.26-.58A12.5 12.5 0 1028.8 37.5a13 13 0 001.304-.067Z"/></svg>
-				No Event Participant
+				No match!
 			</div>
 		`,
         loadingRecords: `
@@ -388,7 +429,6 @@ const updateEventTerm = (event, idx) => {
 const getEventsOptions = async () => {
     try {
         events.value = await getEvents(null, { current: true });
-        console.log("raw events val: ", events.value);
 
         // if `getEvents` returns a falsy value, indicate error
         if (!events.value) {
@@ -405,6 +445,10 @@ const getEventsOptions = async () => {
         events.value = events.value.events.map((event) => ({
             id: event.id,
             title: event.title,
+            startDate: event.start_date,
+            endDate: event.end_date,
+            type: event.type,
+            facilitator: event.facilitator,
             room_id: event.room_id,
             host_id: event.host_id,
         }));
@@ -598,6 +642,12 @@ a {
 
 #dropdownBtn {
     justify-content: space-between !important;
+    min-width: 95%;
+}
+
+.optionIcon {
+    min-height: 80px;
+    min-width: 78px;
 }
 
 .chosen-event {
@@ -612,7 +662,7 @@ a {
     background-color: #eee;
     min-width: 225px;
     height: 100% !important;
-    width: 15%;
+    width: 20%;
     text-align: center;
     text-decoration: none;
     box-shadow: 0 4px 15px rgba(0, 0, 0, 0.1);
